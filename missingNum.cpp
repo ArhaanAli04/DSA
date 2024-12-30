919ef738224089c0623bd5a23f0a0f2bb094fde7
ArhaanAli04 <arhaan.ali2004@gmail.com>
2024-12-30 17:09:44 +0530

    dsa

--- missingNum.cpp
+++ missingNum.cpp
@@ -1,6 +1,16 @@
-int missingNumber(vector<int>& nums) {
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
+        if(n<=2){
+            if(nums[1]!=1){
+                return 1;
+            }
+            else{
+                return 2;
+            }
+        }
         for(int i=1;i<n;i++){
             if(nums[i]-nums[i-1]>1){
                 return nums[i]-1;
@@ -8,3 +18,5 @@ int missingNumber(vector<int>& nums) {
         }
         return 0;
     }
+   
+};
